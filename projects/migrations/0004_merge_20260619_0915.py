5e64e104cf9a0d7a01a31c125c5fde14fa685c1e
rusysh22 <rusydani.sh@gmail.com>
2026-06-29 01:24:23 +0800

    fix:enhance file migrate merge conflict

---
+++ projects/migrations/0004_merge_20260619_0915.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0002_project_allow_public_crud_project_followers'),
+        ('projects', '0003_project_cover_image_alter_phasechecklist_order_and_more'),
+    ]
+
+    operations = [
+    ]
